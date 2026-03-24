68b51e05de21f4478ccad3094756305a2b456750
misha366 <misha.moskalenko.jv@gmail.com>
2026-03-24 21:33:16 +0200

    main: wrapped app with Redux Provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,20 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import './styles/index.sass';
 
-import { initSentry } from './common/logger';
+import { initSentry } from './common';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { router } from './router';
+import { store } from './store';
 
 // Initialize Sentry first (before any other code)
 initSentry();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ErrorBoundary>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </Provider>
   </StrictMode>,
 );
